e93c15af731648a8e910582049290eb4af1287c0
Alex Tatarnikov <alex.tatarnikov@qb.studio>
2018-07-05 13:31:16 +0300

    Uncomment route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   namespace :api, defaults: { format: :json } do
-    # devise_for :users, controllers: {sessions: 'api/sessions'}
+    devise_for :users, controllers: {sessions: 'api/sessions'}
     devise_scope :user do
       post '/authentication_tokens/create', to: "authentication_tokens#create"
     end
