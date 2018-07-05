bcf24228bc82f574646908318ff49f9902920cd0
Alex Tatarnikov <alex.tatarnikov@qb.studio>
2018-07-05 13:23:48 +0300

    Fix API sign in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   namespace :api, defaults: { format: :json } do
-    devise_for :users, controllers: {sessions: 'api/sessions'}
+    # devise_for :users, controllers: {sessions: 'api/sessions'}
     devise_scope :user do
-      get 'users/current', to: 'sessions#show'
+      post '/authentication_tokens/create', to: "authentication_tokens#create"
     end
   end
-
 end
